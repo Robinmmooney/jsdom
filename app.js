3645fe220ac8a4cf70104816772998d658d31b9e
Robin Mooney <robinmooney.student@careerdevs.com>
2018-12-18 16:47:09 +0000

    Updated the app.js file to use const queyrSelector h2 and p tags to change the HTML

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-//console.log(wmf);
-
-var books = document.querySelector('#book-list li .name');
-//console.log(books);
-
-books = document.querySelectorAll('#book-list li .name');
-//console.log(books);
+var books = document.querySelectorAll('#book-list li .name');
 
 Array.from(books).forEach(function(book){
-    console.log(book);
-})
+    book.textContent += "(book title)";
+});
+
+const booklist = document.querySelector('#book-list');
+booklist.innerHTML = '<h2>Books and More Books</h2>';
+booklist.innerHTML += '<p> This is how you add HTML</P>';
 
 
 
@@ -20,15 +17,14 @@ Array.from(books).forEach(function(book){
 
 
 
-// var titles = document.getElementsByClassName("title");
-
-// console.log(Array.isArray(titles));
-// console.log(Array.isArray(Array.from(titles)));
 
 
+// const books = document.querySelectorAll('#book-list li .name');
 
-// Array.from(titles).forEach(function(item){
-//     console.log(item);
-// })
+// Array.from(books).forEach(function(book){
+//   book.textContent += ' (Book title)';
+// });
 
-
+// const bookList = document.querySelector('#book-list');
+// bookList.innerHTML = '<h2>Books and more books...</h2>';
+// bookList.innerHTML += '<p>This is how you add HTML content</p>';
