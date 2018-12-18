3d2d52090b8cd659c60c2c1f46dcc96747cd7cca
Robin Mooney <robinmooney.student@careerdevs.com>
2018-12-18 16:17:51 +0000

    Updated the app.js file to include the use of Array.from and .forEach to display the list of books

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-var titles = document.getElementsByClassName("title");
+const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+//console.log(wmf);
 
-console.log(Array.isArray(titles));
-console.log(Array.isArray(Array.from(titles)));
+var books = document.querySelector('#book-list li .name');
+//console.log(books);
 
+books = document.querySelectorAll('#book-list li .name');
+//console.log(books);
 
-
-Array.from(titles).forEach(function(item){
-    console.log(item);
+Array.from(books).forEach(function(book){
+    console.log(book);
 })
+
+
+
+
+
+
+
+
+
+
+// var titles = document.getElementsByClassName("title");
+
+// console.log(Array.isArray(titles));
+// console.log(Array.isArray(Array.from(titles)));
+
+
+
+// Array.from(titles).forEach(function(item){
+//     console.log(item);
+// })
+
 
