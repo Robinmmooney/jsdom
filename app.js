08a30b8defb4fea94bba0d933d548973c406c157
Robin Mooney <robinmooney.student@careerdevs.com>
2018-12-29 15:07:01 +0000

    added to the app.js file to include an eventlsitener that deletes an item by classname

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-var btns = document.querySelectorAll('#book-list .delete');
+const list = document.querySelector('#book-list ul');
 
-Array.from(btns).forEach(function(btn){
-    btn.addEventListener('click', function(e){
-        
-        const li = e.target.parentElement;
-        
-        li.parentNode.removeChild(li);
-        
-    });
-});
-
-const link = document.querySelector('#page-banner a');
-
-link.addEventListener('click' , function(e){
-    e.preventDefault();
-    console.log('navigation to' , e.target.textContent, "was prevented");
-});
+//delete books
+list.addEventListener('click', function(e){
+  if(e.target.className == 'delete'){
+      const li = e.target.parentElement;
+      list.removeChild(li);
+  }  
+})
 
 
 
@@ -26,24 +17,12 @@ link.addEventListener('click' , function(e){
 
 
 
-// const listItems = document.querySelectorAll('#book-listul li');
+// const list = document.querySelector('#book-list ul');
 
-// Array.from(listItems).forEach(function(item){
-//   item.addEventListener('click', (e) => {
-
+// // delete books
+// list.addEventListener('click', (e) => {
+//   if(e.target.className == 'delete'){
 //     const li = e.target.parentElement;
-//     console.log('child element to remove:', li);
-//     console.log('parent element to remove child from:', li.parentElement);
 //     li.parentNode.removeChild(li);
-
-//   });
-// });
-
-// // prevent default behaviour
-
-// const link = document.querySelector('#page-banner a');
-
-// link.addEventListener('click', function(e){
-//   e.preventDefault();
-//   console.log('Navigation to', e.target.textContent, 'was prevented');
+//   }
 // });
