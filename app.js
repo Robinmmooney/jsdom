fb3c36919fc8b39ebb676c00343dd1b78b5d839e
Robin Mooney <robinmooney.student@careerdevs.com>
2018-12-29 15:51:46 +0000

    added an event listener to add an item to the list and prevent a default action

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const list = document.querySelector('#book-list ul');
 
 //delete books
 list.addEventListener('click', function(e){
-  if(e.target.className == 'delete'){
-      const li = e.target.parentElement;
-      list.removeChild(li);
-  }  
-})
+    if(e.terget.className == 'delete'){
+        const li = e.target.parentElement;
+        list.removeChild(li);
+    }
+});
+
+//add book-list
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit',function(e){
+   e.preventDefault() ;
+   const value = addForm.querySelector('input[type="text"]').value;
+   console.log(value);
+});
+
+
+
 
 
 
@@ -25,4 +37,19 @@ list.addEventListener('click', function(e){
 //     const li = e.target.parentElement;
 //     li.parentNode.removeChild(li);
 //   }
+// });
+
+// const forms = document.forms;
+// console.log(forms);
+// console.log(forms['add-book']);
+
+// Array.from(forms).forEach(function(form){
+//   console.log(form);
+// });
+
+// const addForm = forms['add-book'];
+// addForm.addEventListener('submit', function(e){
+//   e.preventDefault();
+//   const value = addForm.querySelector('input[type="text"]').value;
+//   console.log(value);
 // });
